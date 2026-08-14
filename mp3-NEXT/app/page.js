67bc19abf2e3b461d12070cc2cfc7e4cbca5cc53
KHALID ABDIGANI <almursal45@gmail.com>
2026-08-14 18:12:15 +0300

    chore: connect frontend to production backend

--- mp3-NEXT/app/page.js
+++ mp3-NEXT/app/page.js
@@ -52,7 +52,7 @@ const [copy, setcopy] =useState(null)
   formdata.append("file", file)
 
   try{
-    const response = await fetch("http://localhost:5000/mp3",
+    const response = await fetch("https://mp3-converter-1-w9hf.onrender.com",
       {
         method:"POST",
         credentials:"include",
@@ -93,7 +93,7 @@ URL.revokeObjectURL(url);
 
   useEffect(() => {
 
-    const socket = io("http://localhost:5000");
+    const socket = io("https://mp3-converter-1-w9hf.onrender.com");
 
     socket.on("progress", (data) => {
 
